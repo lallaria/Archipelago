485574770fb204fe8b7ff798333499b91ce6e413
TreZc0_ <trezc0@gmx.de>
2024-07-29 19:44:06 +0200

    fix click

---
+++ WebHostLib/static/assets/copyToClipboard.js
@@ -0,0 +1,7 @@
+async function copyRoomToClipboard() {
+  await navigator.clipboard.writeText("{{ config['HOST_ADDRESS'] }}:{{ room.last_port }}")
+  document.getElementById("snackbar").classList.add("show");
+  setTimeout(() => {
+    document.getElementById("snackbar").classList.remove("show");
+  }, 3000);
+}
