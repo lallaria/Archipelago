003e2df5ec6298a1b0135327290589d6a67d40c5
TreZc0_ <trezc0@gmx.de>
2024-07-29 19:49:55 +0200

    fix refs

--- WebHostLib/static/assets/copyToClipboard.js
+++ WebHostLib/static/assets/copyToClipboard.js
@@ -1,5 +1,5 @@
-async function copyRoomToClipboard() {
-  await navigator.clipboard.writeText(config['HOST_ADDRESS'] + ":" + room.last_port)
+async function copyRoomToClipboard(host, port) {
+  await navigator.clipboard.writeText(host + ":" + port)
   document.getElementById("snackbar").classList.add("show");
   setTimeout(() => {
     document.getElementById("snackbar").classList.remove("show");
