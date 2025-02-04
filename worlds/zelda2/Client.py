f3d2873805b1daf05a87c9e39c187a9a55a04427
TreZc0_ <trezc0@gmx.de>
2025-02-05 00:02:57 +0100

    fix patch registration in client for z2

--- worlds/zelda2/Client.py
+++ worlds/zelda2/Client.py
@@ -18,7 +18,8 @@ EXPECTED_ROM_NAME = "LEGEND OF ZELDA2"
 
 class Zelda2Client(BizHawkClient):
     game = "Zelda II: The Adventure of Link"
-    system = ("NES")
+    system = "NES"
+    patch_suffix = ".apz2"
     location_map = location_table
     npc_locations = special_locations
 
